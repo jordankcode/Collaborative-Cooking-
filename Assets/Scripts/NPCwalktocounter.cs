575bcba5b4294f8ec37c3edc18aa951c9e048e72
jordankcode <monkefin@gmail.com>
2026-07-03 13:48:07 +0930

    npc

--- Assets/Scripts/NPCwalktocounter.cs
+++ Assets/Scripts/NPCwalktocounter.cs
@@ -3,20 +3,41 @@ using UnityEngine.AI;
 
 public class NPCwalktocounter : MonoBehaviour
 {
-    public Transform counterPoint;
+    public Transform pointA;
+    public Transform pointB;
+
     private NavMeshAgent agent;
     private Animator animator;
+    private Transform currentTarget;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-        agent.SetDestination(counterPoint.position);
+
+       
+        currentTarget = pointA;
+        agent.SetDestination(pointA.position);
     }
 
     void Update()
     {
         bool isMoving = agent.velocity.magnitude > 0.1f;
         animator.SetBool("IsWalking", isMoving);
+    }
+
+    void OnMouseDown()
+    {
+       
+        if (currentTarget == pointA)
+        {
+            currentTarget = pointB;
+            agent.SetDestination(pointB.position);
+        }
+        else
+        {
+            currentTarget = pointA;
+            agent.SetDestination(pointA.position);
+        }
     }
 }
